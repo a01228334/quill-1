b92656ad84cc64a73313cafbdda9d9cee67d7ec0
carlosrodrisa0 <a01228334@itesm.mx>
2023-10-07 19:40:43 -0600

    stipe webHooks

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -7,6 +7,7 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2023-08-16',
   typescript: true,
 })
+//stripe
 
 export async function getUserSubscriptionPlan() {
   const { getUser } = getKindeServerSession()
